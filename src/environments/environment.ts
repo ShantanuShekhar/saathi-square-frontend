e035d7ac3afa5a5b89a03a034dc95089864f6e51
Shantanu Shekhar <shantanushekar-at-012857119299>
2026-03-05 16:54:41 +0530

    change uat environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,15 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:1010/auth',
-  societyBaseUrl: 'http://localhost:1010/society/api/societies',
-  residentBaseUrl: 'http://localhost:1010/resident/api/residents',
-  complaintBaseUrl: 'http://localhost:1010/complaint/api/complaints',
-  visitorBaseUrl: 'http://localhost:1010/visitor/api/visitors'
+  // apiBaseUrl: 'http://localhost:1010/auth',
+  // societyBaseUrl: 'http://localhost:1010/society/api/societies',
+  // residentBaseUrl: 'http://localhost:1010/resident/api/residents',
+  // complaintBaseUrl: 'http://localhost:1010/complaint/api/complaints',
+  // visitorBaseUrl: 'http://localhost:1010/visitor/api/visitors'
+
+
+  apiBaseUrl: 'http://3.7.128.155:2020/auth',
+  societyBaseUrl: 'http://3.7.128.155:2020/society/api/societies',
+  residentBaseUrl: 'http://3.7.128.155:2020/resident/api/residents',
+  complaintBaseUrl: 'http://3.7.128.155:2020/complaint/api/complaints',
+  visitorBaseUrl: 'http://3.7.128.155:2020/visitor/api/visitors'
 };
